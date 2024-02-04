198dd4e37b6be5c621e957b9b1bb792070e7a597
Matt Butterfield <matt@mattbutterfield.com>
2024-02-04 09:35:59 -0500

    skip virtual rides

--- combine.py
+++ combine.py
@@ -15,7 +15,8 @@ def main():
     count = 0
     for f in glob.glob(ACTIVITIES_GLOB):
         if f.endswith("fit.gz"):
-            coords.append(get_fit_coords(f))
+            if fit_result := get_fit_coords(f):
+                coords.append(fit_result)
         else:
             coords.append(get_gpx_coords(f))
         count += 1
@@ -40,10 +41,17 @@ def get_gpx_coords(path: str):
     return [[float(pt.attrib["lon"]), float(pt.attrib["lat"])] for pt in pts]
 
 
-def get_fit_coords(path: str):
+def get_fit_coords(path: str) -> list[list[float]] | None:
     pts: list[list[float]] = []
     with gzip.open(path, "r") as f:
         fitfile = fitparse.FitFile(f.read())
+
+        # skip virtual rides
+        if file_ids := [m for m in fitfile.get_messages("file_id")]:
+            if file_ids[0].get_value("manufacturer") == "zwift":
+                print(f"skipping zwift ride: {path}")
+                return
+
         for record in fitfile.get_messages("record"):
             lat = record.get_value("position_lat")
             long = record.get_value("position_long")
